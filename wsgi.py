6aa25ee605ea318dba9ead7fc3e87b0f2277b802
SM <sergey.mikhalyuk@softpark.kz>
2024-09-06 18:09:00 +0500

    fix(wsgi): added infinite loop to make server restart after exception

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,14 @@
-import logging
+import time
 import configparser
 from fiscal_network import app
 from waitress import serve
+from logs import logger as log
 
+log.setup_logger('fiscal.log')
+logger = log.get_logger(__name__)
 # Настройка логгирования
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
 
 # Чтение конфигурации
 config = configparser.ConfigParser()
@@ -18,9 +21,13 @@ port = config.getint('server', 'port', fallback=5000)
 # host = '0.0.0.0'
 # port = 5000
 
+RETRY_DELAY = 1  # Время задержки перед повторной попыткой в секундах
+
 if __name__ == "__main__":
-    logger.info(f"Запуск сервера на http://{host}:{port}")
-    try:
-        serve(app, host=host, port=port)
-    except Exception as e:
-        logger.error(f"Ошибка при запуске сервера: {e}")
+    while True:
+        logger.info(f"Запуск сервера на http://{host}:{port}")
+        try:
+            serve(app, host=host, port=port)
+        except Exception as e:
+            logger.error(f"Ошибка при запуске сервера: {e}")
+            time.sleep(RETRY_DELAY)  # Задержка перед повторной попыткой
